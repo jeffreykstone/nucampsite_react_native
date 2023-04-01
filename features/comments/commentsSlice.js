8774fa42f7e9e123616800d505e9971996c13bb7
Jeff "Gent" Stone <jeffreykstone@gmail.com>
2023-04-01 12:24:18 -0700

    Workshop 2 Create Modal w/form to allow user to submit rating

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,12 +1,14 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { baseUrl } from "../../shared/baseUrl";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../shared/baseUrl';
 
 export const fetchComments = createAsyncThunk(
-    "comments/fetchComments",
+    'comments/fetchComments',
     async () => {
-        const response = await fetch(baseUrl + "comments");
+        const response = await fetch(baseUrl + 'comments');
         if (!response.ok) {
-            return Promise.reject("Unable to fetch, status: " + response.status);
+            return Promise.reject(
+                'Unable to fetch, status: ' + response.status
+            );
         }
         const data = await response.json();
         return data;
@@ -14,28 +16,25 @@ export const fetchComments = createAsyncThunk(
 );
 
 export const postComment = createAsyncThunk(
-    "comments/postComment",
+    'comments/postComment',
     async (payload, { dispatch, getState }) => {
         setTimeout(() => {
             const { comments } = getState();
-            const date = new Date();
-            payload = {
-                ...payload,
-                date: date.toISOString(),
-                id: comments.commentsArray.length,
-            };
+            payload.date = new Date().toISOString();
+            payload.id = comments.commentsArray.length;
+
             dispatch(addComment(payload));
         }, 2000);
     }
 );
 
 const commentsSlice = createSlice({
-    name: "comments",
+    name: 'comments',
     initialState: { isLoading: true, errMess: null, commentsArray: [] },
     reducers: {
         addComment: (state, action) => {
             state.commentsArray.push(action.payload);
-        },
+        }
     },
     extraReducers: (builder) => {
         builder
@@ -49,10 +48,12 @@ const commentsSlice = createSlice({
             })
             .addCase(fetchComments.rejected, (state, action) => {
                 state.isLoading = false;
-                state.errMess = action.error ? action.error.message : "Fetch failed";
+                state.errMess = action.error
+                    ? action.error.message
+                    : 'Fetch failed';
             });
-    },
+    }
 });
 
-export const commentsReducer = commentsSlice.reducer;
 export const { addComment } = commentsSlice.actions;
+export const commentsReducer = commentsSlice.reducer;
